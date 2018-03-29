ce3961d121ae2a3ad5605501d2ba39c1b6d98fed
Valérie <samuelpilcer@gmail.com>
2018-03-29 16:33:58 +0200

    Push first version

---
+++ DCGAN/run_GAN_training.py
@@ -0,0 +1,82 @@
+import numpy as np
+import time
+from tensorflow.examples.tutorials.mnist import input_data
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Reshape
+from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
+from keras.layers import LeakyReLU, Dropout
+from keras.layers import BatchNormalization
+from keras.optimizers import Adam, RMSprop
+
+import matplotlib.pyplot as plt
+
+from GAN import DCGAN
+
+
+class MNIST_DCGAN(object):
+    def __init__(self):
+        self.img_rows = 28
+        self.img_cols = 28
+        self.channel = 1
+
+        self.x_train = input_data.read_data_sets("mnist",\
+            one_hot=True).train.images
+        self.x_train = self.x_train.reshape(-1, self.img_rows,\
+            self.img_cols, 1).astype(np.float32)
+
+        self.DCGAN = DCGAN()
+        self.discriminator =  self.DCGAN.discriminator_model()
+        self.adversarial = self.DCGAN.adversarial_model()
+        self.generator = self.DCGAN.generator()
+
+    def train(self, train_steps=2000, batch_size=256, save_interval=0):
+        noise_input = None
+        if save_interval>0:
+            noise_input = np.random.uniform(-1.0, 1.0, size=[16, 100])
+        for i in range(train_steps):
+            images_train = self.x_train[np.random.randint(0,
+                self.x_train.shape[0], size=batch_size), :, :, :]
+            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+            images_fake = self.generator.predict(noise)
+            x = np.concatenate((images_train, images_fake))
+            y = np.ones([2*batch_size, 1])
+            y[batch_size:, :] = 0
+            d_loss = self.discriminator.train_on_batch(x, y)
+
+            y = np.ones([batch_size, 1])
+            noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 100])
+            a_loss = self.adversarial.train_on_batch(noise, y)
+            log_mesg = "%d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
+            log_mesg = "%s  [A loss: %f, acc: %f]" % (log_mesg, a_loss[0], a_loss[1])
+            print(log_mesg)
+            if save_interval>0:
+                if (i+1)%save_interval==0:
+                    self.plot_images(save2file=True, samples=noise_input.shape[0],\
+                        noise=noise_input, step=(i+1))
+
+    def plot_images(self, save2file=False, fake=True, samples=16, noise=None, step=0):
+        filename = 'mnist.png'
+        if fake:
+            if noise is None:
+                noise = np.random.uniform(-1.0, 1.0, size=[samples, 100])
+            else:
+                filename = "mnist_%d.png" % step
+            images = self.generator.predict(noise)
+        else:
+            i = np.random.randint(0, self.x_train.shape[0], samples)
+            images = self.x_train[i, :, :, :]
+
+        plt.figure(figsize=(10,10))
+        for i in range(images.shape[0]):
+            plt.subplot(4, 4, i+1)
+            image = images[i, :, :, :]
+            image = np.reshape(image, [self.img_rows, self.img_cols])
+            plt.imshow(image, cmap='gray')
+            plt.axis('off')
+        plt.tight_layout()
+        if save2file:
+            plt.savefig(filename)
+            plt.close('all')
+        else:
+            plt.show()
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Generative-Adversarial-Network
-Keras implementation of Generative Adversarial Networks, with features to enable step-by-step CPU training
+
+Keras implementation of Generative Adversarial Networks running on light infrastructure (computer, CPU instance, ...). We provide an opensource implementation of:
+
+- A class DCGAN.GAN to create a Deep Convolutional GAN, based on * Unsupervised representation learning with deep convolutional generative adversarial networks* (Radford et al.).
+
+- A method DCGAN.run_GAN_training that iterates *epochs* RMSprop iterations on the Generative Adversarial Network, on a given dataset. It saves the model and prints a set of generated images frequently to help you monitor training and re-start it through DCGAN.continue_GAN_training if there is a bug/if you need to stop your machine. GAN training can be extremely long!
